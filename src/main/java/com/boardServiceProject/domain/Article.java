f669feed585f94470a52306f37df7205c3ecfad5
Myungho <rlaaudgh1003@daum.net>
2022-09-04 13:19:16 +0900

    #3 CRUD TEST(게시글, 댓글) * JPA + MYSQL + H2 * 의존성 추가

--- src/main/java/com/boardServiceProject/domain/Article.java
+++ src/main/java/com/boardServiceProject/domain/Article.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 import javax.persistence.*;
@@ -22,7 +23,7 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createBy")
         })
-
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/boardServiceProject/JpaRepositoryTest.java
+++ src/test/java/com/boardServiceProject/JpaRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.boardServiceProject;
 
 import com.boardServiceProject.config.JpaConfig;
+import com.boardServiceProject.domain.Article;
 import com.boardServiceProject.repositorry.ArticleCommentRepository;
 import com.boardServiceProject.repositorry.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Jpa Connect Test")
 @Import(JpaConfig.class)
@@ -31,8 +36,57 @@ public class JpaRepositoryTest {
     void givenTestData_whenSelection_thenWorksFine() {
         // Given
 
+
         // When
+        List<Article> articleList = articleRepository.findAll();
 
         // Then
+        assertThat(articleList)
+                .isNotNull()
+                .hasSize(120);
+    }
+    @DisplayName("Insert test")
+    @Test
+    void givenTestData_whenInsert_thenWorkFine() {
+        // Given
+        long previousCount = articleRepository.count();
+
+        // When
+        Article article = articleRepository.save(Article.of("new article", "new Content", "#test"));
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+
+    }
+
+    @DisplayName("Updateing test")
+    @Test
+    void givenTestData_whenUpdate_thenWorkFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updateHashtag = "#springboot";
+        article.setHashtag(updateHashtag);
+        // When
+        Article updatearticle = articleRepository.saveAndFlush(article);
+        // h2는 종료되면 초기화되기 때문에 rollback 되기 때문에 saveAndFlush를 해야 반영
+        // db에 반영하려 하지만 결과에 영향은 없음
+
+        // Then
+        assertThat(updatearticle).hasFieldOrPropertyWithValue("hashtag", updateHashtag);
+    }
+    @DisplayName("Deleteing test")
+    @Test
+    void givenTestData_whenDelete_thenWorkFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousCount = articleRepository.count();
+        long previousCommentCount = articleCommentRepository.count();
+        int deleteCommentsize = article.getArticleComments().size();
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousCommentCount - deleteCommentsize);
+
     }
 }
