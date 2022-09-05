7ad037af3e19cb08e6381f30d8c3a430187f3adb
Myungho <rlaaudgh1003@daum.net>
2022-09-06 06:22:25 +0900

    #4 Page Controller(Thymeleaf) * thymeleaf, configuration-processor 의존성 추가

---
+++ src/main/java/controller/ArticleController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        // articles에 있는 것들을 리스트로 호출
+        return "articles/index";
+    }
+}
---
+++ src/test/java/com/boardServiceProject/controller/ArticleControllerTest.java
@@ -0,0 +1,68 @@
+package com.boardServiceProject.controller;
+
+import controller.ArticleController;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View Controller - Article")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[view][GET] Article List - Success")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnArticesView() throws Exception{
+    mvc.perform(get("/articles"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // HTML과 호환되는 html도 Pass
+            .andExpect(view().name("articles/index")) // 인덱스가 있는지 검사
+            .andExpect(model().attributeExists("articles"));
+}
+
+    @Disabled("Need for development")
+    @DisplayName("[view][GET] Article One - Success")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnArticeView() throws Exception{
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail")) // 인덱스가 있는지 검사
+                .andExpect(model().attributeExists("articleComments"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @Disabled("Need for development")
+    @DisplayName("[view][GET] Article Search - Success")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticeView() throws Exception{
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+    @Disabled("Need for development")
+    @DisplayName("[view][GET] Hashtag Search - Success")
+    @Test
+    public void givenNothing_whenRequestingHashtagSearchView_thenReturnArticeView() throws Exception{
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("articles/search-hashtag"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
